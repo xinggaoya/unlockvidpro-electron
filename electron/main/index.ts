f2fca5952ac5c05536cab4d37768d7a69fabfdc0
XingGao <1032227283@qq.com>
2024-01-09 17:20:15 +0800

    更新APP图标

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -46,10 +46,10 @@ const indexHtml = join(process.env.DIST, 'index.html')
 
 async function createWindow() {
     win = new BrowserWindow({
-        title: 'Main window',
+        title: 'UnlockVid Pro',
         width: 1200,
         height: 800,
-        icon: join(process.env.VITE_PUBLIC, 'favicon.ico'),
+        icon: join(process.env.VITE_PUBLIC, 'logo.svg'),
         webPreferences: {
             preload,
             // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
